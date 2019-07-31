6de688ce5b7825613b184ae1baee8f600d144f70
Mykhayl <tserr.mykhayl@apriorit.com>
2019-07-31 23:36:45 +0300

    fixed When_MultipleToFourYear_ReturnTrue

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -16,12 +16,12 @@ If your language provides a method in the standard library that does this look-u
 #include <ctime>
 #include <cstdlib>
 
-bool is_leap_year(int year) {
-    return year % 400;
+bool is_leap_year(unsigned year) {
+    return year % 4 == 0;
 }
 
-TEST(LeapYearTest, When_MultipleToFourHundredYear_ReturnTrue) {
+TEST(LeapYearTest, When_MultipleToFourYear_ReturnTrue) {
     srand(time(0));
-    int random_year = rand() * 400;
+    int random_year = rand() * 4;
     ASSERT_EQ(true, is_leap_year(random_year));
 }
